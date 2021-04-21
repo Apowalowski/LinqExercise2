53b67436a5eef6b03a199a648b1b00fc93111a17
Apowalowski <acpowalowski@gmail.com>
2021-04-21 14:43:08 -0400

    LinqExercise2

---
+++ LinqExercise2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace LinqExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> videoGames = new List<string>
+            { "The last of us", "Destiny,", "Super Smash Bros", "Death Stranding", "Call of Duty: Modern Warfare", "Grand Theft Auto"};
+           IEnumerable<string> longGameNames = videoGames.OrderByDescending(x => x.Length);
+            foreach(var videoGame in longGameNames)
+            {
+                Console.WriteLine(videoGame);
+            }
+            
+        }
+    }
+}
